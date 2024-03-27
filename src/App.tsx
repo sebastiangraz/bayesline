e09db7899177668edccd575cb52a32962da604f3
Sebastian Graz <graz@live.se>
2024-03-27 21:50:17 +0100

    Add grid and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import style from './utils.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
+    <main>
+      <div className="tile"></div>
+      <div className={`${style.section}`}>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Bayesline + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className={style.section}>
+        <div className="s-6 e-12">
+          <h1>A new era of financial analytics</h1>
+        </div>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+
+      <div className={style.section}>
+        <div className="e-6 sub">
+          <h1 className="e-4">A new era of financial analytics</h1>
+        </div>
+      </div>
+
+      <div className={style.section}>
+        <div className="e-6 sub">
+          <h2 className="e-2">card</h2>
+          <h2 className="s-2 e-4">card</h2>
+          <h2 className="s-4 e-6">card</h2>
+        </div>
+        <div className="s-8 e-12 sub">
+          <h2 className="e-2">card</h2>
+          <h2 className="s-2 e-4">card</h2>
+        </div>
+      </div>
+
+      <div className={style.section}>
+        <div className="s-2 e-10 sub">
+          <h1 className="s-2 e-6">center sub text</h1>
+        </div>
+      </div>
+
+      <div className={style.section}>
+        <div className="e-4">
+          <h1>A new era of center</h1>
+        </div>
+        <div className="s-4 e-12">
+          <h1>A new era of center</h1>
+        </div>
+      </div>
+
+      <div className={style.section}>
+        <div className="e-8">
+          <h1>A new era of financial analytics</h1>
+        </div>
+        <div className="s-8 e-12">
+          <h1>A new era of financial analytics</h1>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { patchCssModules } from 'vite-css-modules';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  //use patchCssModules until VITE 5.3 is released
+  plugins: [patchCssModules(), react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly'
+    }
+  }
+});
