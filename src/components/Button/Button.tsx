833418baff3e89fec7ad4a4b88fd78cc71a9c590
Sebastian Graz <graz@live.se>
2024-06-19 21:29:12 +0200

    Add colors

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -50,7 +50,7 @@ export const Button = (props: ButtonProps) => {
 
   const backgroundColor = rest.bgColor ? rest.bgColor : '';
   const foregroundColor = rest.fgColor ? rest.fgColor : '';
-  const nodeColor = rest.nodeColor ? rest.nodeColor : 'var(--blue)';
+  const nodeColor = rest.nodeColor ? rest.nodeColor : 'var(--brand-2)';
   const buttonStyle = `${style.button} ${style[background]} ${background} ${className}`;
 
   if (rest.to) {
