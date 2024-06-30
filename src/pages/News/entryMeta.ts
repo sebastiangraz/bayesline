4ed9ad691700ae2f23e6a0eb6827b97b3c538629
Sebastian Graz <graz@live.se>
2024-06-30 16:33:36 +0200

    use folder structure for entries

--- src/pages/News/entryMeta.ts
+++ src/pages/News/entryMeta.ts
@@ -1,18 +1,15 @@
 import type { Toc } from '@stefanprobst/rehype-extract-toc';
+import { getPrevPathFromExtension } from '@/helpers/utils';
 
 const globEntries = Object.entries(
-  import.meta.glob<string | string[] | any>(['@/pages/entries/*.mdx'], {
+  import.meta.glob<string | string[] | any>(['@/pages/entries/*/*.mdx'], {
     eager: true
   })
 );
 
-function getFileName(path: string) {
-  return path.split('/').pop()?.replace('.mdx', '');
-}
-
 export const entryMeta = globEntries.map(([url, module]) => {
   const Page = module.default;
-  const fileName = getFileName(url);
+  const fileName = getPrevPathFromExtension(url, '.mdx');
   const title = module.frontmatter?.title || fileName;
   const excerpt = module.frontmatter?.excerpt;
   const theme = module.frontmatter?.theme;
