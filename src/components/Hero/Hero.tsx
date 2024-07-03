01d100072bbd605bc8393c29b7a4afc5320a04e1
sebastiangraz <graz@live.se>
2024-07-03 14:14:21 +0200

    add svg padding

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,10 +6,12 @@ import hero from '@/assets/homepage-hero.png';
 import { useRef } from 'react';
 
 const VectorField = () => {
-  const size = 288; // Size of the SVG canvas
-  const numArrows = 12; // Number of arrows along one dimension
+  const svgSize = 288; // Size of the SVG canvas
+  const svgPadding = 16; // Padding around the canvas
+  const size = svgSize - svgPadding * 2; // Size of the vector field
 
-  const arrowPadding = 12; // Padding between arrows
+  const numArrows = 12; // Number of arrows along one dimension
+  const arrowPadding = 10; // Padding between arrows
   const arrowSize = (size - arrowPadding * (numArrows - 1)) / numArrows;
 
   const mouseX = useMotionValue(size / 2);
@@ -18,30 +20,40 @@ const VectorField = () => {
 
   const handleMouseMove = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     const rect = svgRef.current!.getBoundingClientRect();
-    if (rect) {
-      mouseX.set(event.clientX - rect.left);
-      mouseY.set(event.clientY - rect.top);
-    }
+    mouseX.set(event.clientX - rect.left);
+    mouseY.set(event.clientY - rect.top);
   };
 
   const arrows = Array.from({ length: numArrows * numArrows }, (_, index) => {
-    const x = (index % numArrows) * (arrowSize + arrowPadding);
-    const y = Math.floor(index / numArrows) * (arrowSize + arrowPadding);
+    const x = (index % numArrows) * (arrowSize + arrowPadding) + svgPadding + arrowSize / 2;
+    const y = Math.floor(index / numArrows) * (arrowSize + arrowPadding) + svgPadding + arrowSize / 2;
 
     const angle = useTransform<number, number>([mouseX, mouseY], ([latestX, latestY]) => {
       return Math.atan2(latestY - y, latestX - x) * (180 / Math.PI);
     });
-    return { x, y, angle };
+
+    // Use spring to animate the end points of the lines
+    const x2 = useSpring(
+      useTransform(angle, (a) => x + arrowSize * Math.cos((a * Math.PI) / 180)),
+      { stiffness: 400, damping: 20 }
+    );
+    const y2 = useSpring(
+      useTransform(angle, (a) => y + arrowSize * Math.sin((a * Math.PI) / 180)),
+      { stiffness: 400, damping: 20 }
+    );
+
+    return { x, y, x2, y2 };
   });
+
   return (
-    <svg ref={svgRef} width={size} height={size} onMouseMove={handleMouseMove}>
+    <svg ref={svgRef} width={svgSize} height={svgSize} onMouseMove={handleMouseMove}>
       {arrows.map((arrow, index) => (
         <motion.line
           key={index}
           x1={arrow.x}
           y1={arrow.y}
-          x2={useTransform(arrow.angle, (a) => arrow.x + arrowSize * Math.cos((a * Math.PI) / 180))}
-          y2={useTransform(arrow.angle, (a) => arrow.y + arrowSize * Math.sin((a * Math.PI) / 180))}
+          x2={arrow.x2}
+          y2={arrow.y2}
           stroke="var(--background-3)"
           strokeWidth="1"
           initial={{ opacity: 0 }}
