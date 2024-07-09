ed0c4cdd77af70fd62598f112ab361f35bd6b42d
sebastiangraz <graz@live.se>
2024-07-09 20:04:01 +0200

    Reset test

--- src/pages/Brand/index.tsx
+++ src/pages/Brand/index.tsx
@@ -15,32 +15,31 @@ export function Brand() {
         <Card dataTheme={2}>
           <div className="card-header">
             <Text.H4>Vector Fields</Text.H4>
+            <Flex wrap>
+              <VectorField variant="swirl" />
+              <VectorField variant="straight" />
+              <VectorField variant="radial" />
+              <VectorField variant="checker" />
+              <VectorField variant="grid" />
+              <VectorField variant="magnify" />
+            </Flex>
           </div>
-          <Flex wrap>
-            <VectorField variant="swirl" />
-            <VectorField variant="straight" />
-            <VectorField variant="radial" />
-            <VectorField variant="checker" />
-            <VectorField variant="grid" />
-            <VectorField variant="magnify" />
-            <ShapeField isStatic variant="pcb" />
-          </Flex>
         </Card>
-        <Card.Primary dataTheme={2}>
+        <Card.Secondary dataTheme={2}>
           <div className="card-header">
             <Text.H4>Shape Fields</Text.H4>
-            <ShapeField isStatic variant="swirl" />
-            <ShapeField isStatic variant="dithered-gradient" />
-            <ShapeField isStatic variant="radial" />
             <ShapeField isStatic variant="bayesian" rows={55} columns={55} padding={1} />
             <ShapeField isStatic variant="bayesian" rows={24} columns={40} padding={1} />
             <ShapeField isStatic variant="bayesian" rows={55} columns={21} padding={1} />
             <ShapeField isStatic variant="bayesian" rows={28} columns={28} padding={1} />
+            <ShapeField isStatic variant="swirl" />
+            <ShapeField isStatic variant="dithered-gradient" />
+            <ShapeField isStatic variant="radial" />
+
             <ShapeField isStatic variant="checker" />
             <ShapeField isStatic variant="pcb" />
-            <VectorField variant="magnify" />
           </div>
-        </Card.Primary>
+        </Card.Secondary>
       </Cards>
     </div>
   );
