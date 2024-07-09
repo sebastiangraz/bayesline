c2f595a981f7f50aad2fec8e531b6afdfa057cb8
sebastiangraz <graz@live.se>
2024-07-09 12:42:02 +0200

    refactor shape

--- src/components/VectorField/ShapeField.tsx
+++ src/components/VectorField/ShapeField.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
 
 interface ShapeFieldProps {
@@ -21,24 +21,6 @@ interface ShapeProps {
   opacity: number;
 }
 
-function gaussian(x: number, mean: number, variance: number) {
-  return (1 / Math.sqrt(2 * Math.PI * variance)) * Math.exp(-Math.pow(x - mean, 2) / (2 * variance));
-}
-
-function bayesianCurve(col: number, row: number, columns: number) {
-  const mean = columns / 2;
-  const variance = Math.pow(columns / 7, 3.2); // Adjust this for wider/narrower curves
-  const baseHeight = gaussian(col, mean, variance);
-  const normalizedHeight = baseHeight * row; // Normalize height to fit rows
-  return normalizedHeight;
-}
-const tornado = (col: number, row: number, rows: number, columns: number) => {
-  const angle = Math.atan2(row - rows / 2, col - columns / 2);
-  const radius = Math.sqrt(Math.pow(row - rows / 2, 2) + Math.pow(col - columns / 2, 2));
-  const spiral = Math.sin(angle * 3 + radius / 0.7);
-  return spiral;
-};
-
 const childVariants = {
   hidden: ({ shimmer }: { shimmer: number }) => ({
     opacity: 0,
@@ -57,26 +39,6 @@ const childVariants = {
     }
   })
 };
-
-function pcbPattern(col: number, row: number) {
-  // Variability in the grid spacing
-  let gridSizes = [2, 5, 9, 27, 1, 1, 1];
-  const gridSize = gridSizes[Math.floor(Math.random() * gridSizes.length * 0.2)];
-
-  // Establish connections based on variable grid sizes
-  const connections =
-    col % gridSizes[(col % gridSize) * 1] > gridSize || row % gridSizes[(row % gridSize) * 8] === 0 ? 1 : 0;
-
-  // Looks like generated text
-  // const connections =
-  // col % gridSizes[(col % gridSize) * 20] > gridSize || row % gridSizes[(row % gridSize) * 8] === 0 ? 1 : 0;
-
-  // Looks like icons with text
-  //   const connections =
-  //     col % gridSizes[(row % gridSize) * 2] < 2 || row % gridSizes[gridSize * 12 + col] === 0 ? 0 : 0.6;
-
-  return connections;
-}
 
 const Shape = React.memo(({ x, y, type, size = 8, opacity }: ShapeProps) => {
   if (!type) return null;
@@ -116,7 +78,7 @@ export const ShapeField = React.memo(
       amount: 'some'
     });
 
-    const paddedWidth = width - columns * padding; // Reduce total width by total horizontal padding
+    const paddedWidth = width - columns * padding;
     const paddedHeight = height - rows * padding;
 
     const cellWidth = paddedWidth / columns;
@@ -124,6 +86,63 @@ export const ShapeField = React.memo(
 
     const midX = width / 2 - cellWidth / 2;
     const midY = height / 2 - cellHeight / 2;
+
+    const getShapeType = useCallback(
+      (col: number, row: number, baseNoise: number, noise: number, index: number, y: number) => {
+        let shapeType: ShapeProps['type'];
+        const ydistance = y / height;
+        function combinedType(value: number) {
+          let shapeType: ShapeProps['type'];
+          if (value <= 0.33) {
+            shapeType = 'rect';
+          } else if (value <= 0.66) {
+            shapeType = 'ellipse';
+          } else {
+            shapeType = 'line';
+          }
+          return shapeType;
+        }
+
+        switch (variant) {
+          case 'swirl':
+            shapeType = tornado(col, row, rows, columns) > 0 ? 'line' : 'ellipse';
+            break;
+
+          case 'dithered-gradient':
+            shapeType = combinedType(baseNoise + ydistance);
+            break;
+
+          case 'radial':
+            shapeType = baseNoise + noise <= 0.5 ? 'rect' : 'ellipse';
+            break;
+
+          case 'grid':
+            shapeType = bayesianCurve(col, row, columns) < 0.5 ? 'rect' : 'ellipse';
+            break;
+
+          case 'checker':
+            shapeType = index % 2 === 0 ? 'rect' : 'ellipse';
+            break;
+
+          case 'pcb':
+            shapeType = pcbPattern(col, row) > 0.5 ? 'rect' : 'ellipse';
+            break;
+
+          default:
+            shapeType = 'rect';
+            break;
+        }
+
+        return shapeType;
+      },
+      [columns, height, rows, variant]
+    );
+
+    const getOpacity = useCallback((combinedNoise: number) => {
+      if (combinedNoise <= 0.33) return 0.2;
+      if (combinedNoise <= 0.66) return 0.6;
+      return 1;
+    }, []);
 
     const shapes = React.useMemo(
       () =>
@@ -142,70 +161,18 @@ export const ShapeField = React.memo(
 
           //linear tools
           const dx = col / columns;
-          const dy = row / rows;
+          // const dy = row / rows;
           // const interpolation = Math.abs(rx - ry);
           // const xdistance = x / width;
-          const ydistance = y / height;
+          //const ydistance = y / height;
           //const diagDistance = Math.sqrt(dx * dx + dy * dy);
 
           //noise tools, noice indeed
-          const baseNoise = (0.5 - Math.random()) * 0.66; // Between -0.1 and 0.1
-          const noise = (Math.sin(dx * Math.PI) + Math.cos(ry * Math.PI)) / 2; // Oscillates between -1 and 1, normalized to 0-1
+          const baseNoise = (0.5 - Math.random()) * 0.66;
+          const noise = (Math.sin(dx * Math.PI) + Math.cos(ry * Math.PI)) / 2;
 
-          let shapeType: ShapeProps['type'];
-
-          function combinedType(value: number) {
-            let shapeType: ShapeProps['type'];
-            if (value <= 0.33) {
-              shapeType = 'rect';
-            } else if (value <= 0.66) {
-              shapeType = 'ellipse';
-            } else {
-              shapeType = 'line';
-            }
-            return shapeType;
-          }
-
-          switch (variant) {
-            case 'swirl':
-              // add a twist to the swirl by increasing the index each revolution
-              shapeType = tornado(col, row, rows, columns) > 0 ? 'line' : 'ellipse';
-              break;
-
-            case 'dithered-gradient':
-              shapeType = combinedType(baseNoise + ydistance);
-              break;
-
-            case 'radial':
-              shapeType = baseNoise + noise <= 0.5 ? 'rect' : 'ellipse';
-              break;
-
-            case 'grid':
-              shapeType = bayesianCurve(col, row, columns) < 0.5 ? 'rect' : 'ellipse'; // Alternates every row
-              break;
-
-            case 'checker':
-              shapeType = index % 2 === 0 ? 'rect' : 'ellipse'; // Alternates every cell
-              break;
-
-            case 'pcb':
-              shapeType = pcbPattern(col, row) > 0.5 ? 'rect' : 'ellipse';
-              break;
-
-            default:
-              shapeType = 'rect';
-              break;
-          }
-          let opacity;
-          const combinedNoise = baseNoise + noise;
-          if (combinedNoise <= 0.33) {
-            opacity = 0.2;
-          } else if (combinedNoise <= 0.66) {
-            opacity = 0.6;
-          } else {
-            opacity = 1;
-          }
-
+          const opacity = getOpacity(baseNoise + noise);
+          const shapeType = getShapeType(col, row, baseNoise, noise, index, y);
           const color = shapeType === 'rect' ? color1 : color2;
 
           return (
@@ -221,7 +188,22 @@ export const ShapeField = React.memo(
             </motion.g>
           );
         }),
-      [rows, columns, cellWidth, padding, cellHeight, midX, width, midY, height, variant, color1, color2, isInView]
+      [
+        rows,
+        columns,
+        cellWidth,
+        padding,
+        cellHeight,
+        midX,
+        width,
+        midY,
+        height,
+        getOpacity,
+        getShapeType,
+        color1,
+        color2,
+        isInView
+      ]
     );
     return (
       <motion.svg
@@ -238,3 +220,41 @@ export const ShapeField = React.memo(
     );
   }
 );
+
+function gaussian(x: number, mean: number, variance: number) {
+  return (1 / Math.sqrt(2 * Math.PI * variance)) * Math.exp(-Math.pow(x - mean, 2) / (2 * variance));
+}
+
+function bayesianCurve(col: number, row: number, columns: number) {
+  const mean = columns / 2;
+  const variance = Math.pow(columns / 7, 3.2); // Adjust this for wider/narrower curves
+  const baseHeight = gaussian(col, mean, variance);
+  const normalizedHeight = baseHeight * row; // Normalize height to fit rows
+  return normalizedHeight;
+}
+const tornado = (col: number, row: number, rows: number, columns: number) => {
+  const angle = Math.atan2(row - rows / 2, col - columns / 2);
+  const radius = Math.sqrt(Math.pow(row - rows / 2, 2) + Math.pow(col - columns / 2, 2));
+  const spiral = Math.sin(angle * 3 + radius / 0.7);
+  return spiral;
+};
+
+function pcbPattern(col: number, row: number) {
+  // Variability in the grid spacing
+  let gridSizes = [2, 5, 9, 27, 1, 1, 1];
+  const gridSize = gridSizes[Math.floor(Math.random() * gridSizes.length * 0.2)];
+
+  // Establish connections based on variable grid sizes
+  const connections =
+    col % gridSizes[(col % gridSize) * 1] > gridSize || row % gridSizes[(row % gridSize) * 8] === 0 ? 1 : 0;
+
+  // Looks like generated text
+  // const connections =
+  // col % gridSizes[(col % gridSize) * 20] > gridSize || row % gridSizes[(row % gridSize) * 8] === 0 ? 1 : 0;
+
+  // Looks like icons with text
+  //   const connections =
+  //     col % gridSizes[(row % gridSize) * 2] < 2 || row % gridSizes[gridSize * 12 + col] === 0 ? 0 : 0.6;
+
+  return connections;
+}
