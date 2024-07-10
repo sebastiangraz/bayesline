f7e5d43a43eb9bf7d097c9e5558ac03d0cee3967
sebastiangraz <graz@live.se>
2024-07-10 11:44:27 +0200

    feature work

--- src/pages/Home/Feature/Feature.tsx
+++ src/pages/Home/Feature/Feature.tsx
@@ -1,6 +1,6 @@
 import { StaggerText } from '@/helpers/StaggerText';
 import style from './feature.module.css';
-import { Flex, Text, VectorField } from '@/components';
+import { Button, Flex, Text, VectorField } from '@/components';
 import hero from '@/assets/homepage-hero.png';
 
 interface HeroProps {
@@ -8,26 +8,34 @@ interface HeroProps {
 }
 
 export const Feature = (props: HeroProps) => {
-  const { title = 'Financial analytics rethought.' } = props;
-
   return (
-    <div className={`col bleed`}>
+    <div className={`col bleed ${style.feature}`}>
       <div className={`${style.cue}`}>
-        <Text.H4>Invest in time.</Text.H4>
-        <VectorField />
-        <VectorField variant="twist" />
+        <section>
+          <Text.H5>Invest in time.</Text.H5>
+          <Text.Body>Build model reports 56% faster</Text.Body>
+        </section>
+        <section>
+          <VectorField />
+          <Text.Small>Build model reports 56% faster</Text.Small>
+        </section>
+        <section>
+          <VectorField variant="twist" />
+          <Text.Small>Build model reports 56% faster</Text.Small>
+        </section>
       </div>
-      <div className={`col ${style.feature}`}>
-        <Flex column gap={4}>
-          <Text.H1>
-            <StaggerText>{title}</StaggerText>
-          </Text.H1>
-          <Text.Body>
-            We deliver the analytics that investment managers are already familiar with, but hyper-customizable and
-            blazing fast.
-          </Text.Body>
+      <Flex column gap={4} className={`col ${style.text}`}>
+        <Text.Body balance>
+          We deliver the analytics that investment managers are already familiar with, but hyper-customizable and
+          blazing fast.
+        </Text.Body>
+        <Flex gap={4} center>
+          <Button type="primary" href="https://calendar.app.google/qq4pjZcmDdzTjBme7" target="_blank">
+            <Text.Caps>Book a Demo</Text.Caps>
+          </Button>
+          <Text.Small balance>Be the first to know about our beta launch.</Text.Small>
         </Flex>
-      </div>
+      </Flex>
       <div className={`col ${style.ui}`}>
         <img src={hero} alt="hero" />
       </div>
